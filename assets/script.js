6399d5de10faaf809f4fdd60160cf7757d957b28
JRyan-123 <acostajoseryan4@gmail.com>
2025-06-12 17:15:29 +0800

    ADDING BACKEND, USING JS

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+const lists = document.querySelectorAll('.list'); 
+const leftBox = document.getElementById('left');
+const rightBox = document.getElementById('right');
+
+let selected = null;
+
+for (let list of lists) {
+  list.addEventListener("dragstart", function (e) {
+    selected = e.target;
+  });
+}
+
+
+rightBox.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+rightBox.addEventListener("drop", function (e) {
+  if (selected) {
+    rightBox.appendChild(selected);
+    selected = null;
+  }
+});
+
+
+leftBox.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+leftBox.addEventListener("drop", function (e) {
+  if (selected) {
+    leftBox.appendChild(selected);
+    selected = null;
+  }
+});
